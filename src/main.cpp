164209c66976410c1410535db89d0388e8dd37c1
RyanTavol <121977925+RyanTavol@users.noreply.github.com>
2025-05-25 21:21:19 -0400

    Added basic game manager for handling moves

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,11 @@
 #include <locale>
 
 #include "terminal_chess/ChessBoard.hpp"
+#include "terminal_chess/GameManager.hpp"
 
 using namespace terminal_chess;
 
-int main() {
-  std::cout << "Welcome to Terminal Chess" << "\n";
-  
+void test_1() {
   terminal_chess::ChessBoard board(true);
   std::cout << board.to_string() << "\n";
   Move pawnMove = {{6,4}, {4,4}};
@@ -26,6 +25,20 @@ int main() {
   bool knightResult = board.move_piece(knightMove, Color::White);
   // std::cout << knightResult << "\n";
   std::cout << board.to_string() << "\n";
+}
+
+void test_2() {
+  terminal_chess::GameManager game(true);  
+  game.next_move({{6,4}, {4,4}});
+  game.next_move({{1,4}, {3,4}});
+  game.next_move({{7,6}, {5,5}});
+}
+
+int main() {
+  std::cout << "Welcome to Terminal Chess" << "\n";
+  
+  // test_1();
+  test_2();
 
   return 0;
 }
